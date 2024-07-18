c180ccdebbdac4f62f20b1acb3ba66c472c012b3
Sachin Tewari <mailforsachin@gmail.com>
2024-07-18 10:41:06 -0800

    Created exercise.py file, rectangle perimeter problem

--- exercise.py
+++ exercise.py
@@ -1,13 +1,45 @@
+""" userInput ="Y"
+while(userInput=="y" or userInput =="Y"):
+    
+    rectLen= input("Enter the length of a rectangle. ");
+    rectBdth= input("Enter the bdth of a rectangle. ")
 
-rectLen= input("Enter the length of a rectangle. ");
-rectBdth= input("Enter the bdth of a rectangle. ")
-type
+    if (type(rectLen)!=int and float(rectLen) < 0.1) or (type(rectBdth)!=int and float(rectBdth)<0.1):
+        print("Invalid length or breadth.")
+        userInput=input("Try again, y/n: ");
+        if (userInput =="y" or userInput=="Y"):
+            continue;
+        else:
+            print("Thank you for using the calculator!");
+            exit;
+    else:
+        if (type(rectBdth)!=int or type(rectLen)!=int):
+            print(f"Perimeter is {2*(float(rectBdth)+float(rectLen))}");    
+        else:
+            print(f"Perimeter is {2*(int(rectBdth)+int(rectLen))}");
+        userInput=input("Try again, y/n: ");
+        if (userInput =="y" or userInput=="Y"):
+            continue;
+        else:
+            print("Thank you for using the calculator");
+            exit;
 
-if (type(rectLen)!=int and rectLen <0.1) or (type(rectBdth)!=int):
-    print("Invalid length or breadth. Please try again")
-else:
-    print("Perimeter is %d", 2*(rectBdth+rectLen));
+ """
 
+def calc_perimeter(rectBr,rectHt):
+    return 2*float(rectBr)+2*float(rectHt);
 
+userInput=input("Welcome to the calculator game! Do you want to continue(y/n): ")
 
-
+while userInput.lower()=="y":
+    try:
+        rectBr=input("Enter the breadth: ");
+        rectHt=input("Enter the height: ");
+        print(f"Perimeter is {calc_perimeter(rectBr,rectHt)}");
+    except TypeError:
+        print(" Values must be number. ");
+        continue;
+    except ValueError:
+        print(" Values must be number.");
+        continue;
+    userInput=input("Do you want to continue?(y/n)  ");

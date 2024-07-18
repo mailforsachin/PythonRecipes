e5390156a0b9694e092683fcf2b4744953a538d8
Sachin Tewari <mailforsachin@gmail.com>
2024-07-17 16:23:50 -0800

    Created exercise.py file, rectangle perimeter problem

---
+++ exercise.py
@@ -0,0 +1,13 @@
+
+rectLen= input("Enter the length of a rectangle. ");
+rectBdth= input("Enter the bdth of a rectangle. ")
+type
+
+if (type(rectLen)!=int and rectLen <0.1) or (type(rectBdth)!=int):
+    print("Invalid length or breadth. Please try again")
+else:
+    print("Perimeter is %d", 2*(rectBdth+rectLen));
+
+
+
+

aa185e1848e11584e8fddaa4dd74cd9445a92e9f
Sachin Tewari <mailforsachin@gmail.com>
2024-07-21 11:31:51 -0800

    Created first_test.py file, July 21 noon, first_test

--- first_test.py
+++ first_test.py
@@ -67,3 +67,140 @@ while (int(my_counter) <=10):
     else:
         print(i+1); """
 
+
+""" def whats_my_name(name):
+    print(f"Hello {name}");
+
+enter_your_name = input("Whats your name? ")
+whats_my_name(enter_your_name);
+ """
+
+""" def sum(a,b):
+    return print(a+b);
+
+try:
+    enter_your_two_numbers = input ("Enter two numbers followed by spaces. ")
+    enter_your_two_numbers = enter_your_two_numbers.split();
+    sum(int(enter_your_two_numbers[0]), int(enter_your_two_numbers[1]));
+except NameError:
+    print("Enter numbers please")
+
+except ValueError:
+    print("Enter numeric value please") """
+
+""" 
+try:
+    x=input("Enter your number. ")
+    final_number= lambda x:x+10;
+    print(final_number(int(x)));
+except NameError:
+    print("Incorrect Identifier");
+except ValueError:
+    print("Incorrect Value") """
+
+
+""" my_list = [1,2,3,4,5]
+
+my_new_list=list(map(lambda x:x+1,my_list))
+
+print(my_new_list) """
+""" 
+my_list =[];
+
+def add_to_list(new_num):
+    my_list.append(new_num);
+    print(my_list)
+
+def remove_from_list():
+    my_list.pop()
+    print(my_list)
+
+def sort_the_list():
+    my_list.sort();
+    print(my_list)
+
+def find_max_from_list():
+    return print (max(my_list))
+
+your_ans="y"
+while (your_ans.lower()=="y"):
+
+    
+    my_task= input("What do you wana do? add-1,remove-2,find_max-3, sort-4")
+
+    if (int(my_task)==1):
+        my_num=input("Enter the number. ")
+        add_to_list(my_num)
+    elif (int(my_task)==2):
+        remove_from_list()
+    elif (int(my_task)==3):
+        find_max_from_list();
+    else:
+        sort_the_list()
+    
+    your_ans=input("Do you want to continue?(Y/N)") """
+
+
+""" #List comprehension
+
+my_list=[1,2,3,4,5]
+my_new_list=[num*2 for num in my_list]
+print(my_new_list) """
+
+""" my_list=["sachin", "tewari"]
+my_new_list=[my_list[::-1] for my_list in my_list]
+print(my_new_list) """
+
+""" my_list =[1,2,3,4,5]
+my_new_list = [num for num in my_list if num %2==0 ]
+print(my_new_list) """
+
+#curly dict, rountuple, squarelist
+
+""" my_dict = ("this", 1, "is", True);
+print(type(my_dict))
+print(list(my_dict));
+print(type(my_dict)) """
+
+""" 
+#set doesnt allow duplicates
+my_set=set(["1","1","1"])
+print(my_set) """
+
+#dictionary
+""" 
+my_dictionary = {"Leader":{"name": "John", "age": 28, "occupation": "teacher"}, "Doctor" :{"name": "Jane", "age": 32, "occupation": "doctor"}, 
+"Engineer": {"name": "Bob", "age": 25, "occupation": "engineer"}}
+
+print(type(my_dictionary));
+#print(my_dictionary["Leader"]["name"]);
+
+for key, value in my_dictionary.items():
+    print (value["name"])
+ """
+#vowel or not
+
+""" my_string = "aerious"
+
+my_vowel_count=0;
+my_vowel_list =[];
+for strings in my_string:
+    if strings in {"a","e", "i", "o", "u"}:
+        if strings not in my_vowel_list:
+           my_vowel_count=my_vowel_count+1;
+           my_vowel_list.append(strings)
+print(my_vowel_count)
+print(my_vowel_list) """
+
+#palindrome
+""" 
+input_str="saippuakivikauppias"
+check_str=input_str[::-1]
+if (input_str==check_str):
+   print("Palindrome")
+else:
+    print("Not a palindrome")
+
+#Alt
+input_str="saippuakivikauppias"
+print("Palindrome" if input_str == input_str[::-1] else "Not a palindrome")  """

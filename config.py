b84c950a422f1ef0f7b7d28a883abc2efc3bb677
Sachin Tewari <mailforsachin@gmail.com>
2024-07-30 16:52:21 -0800

    Update config.py
    Removed secret

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DATABASE_LOCATION="c:\Database\my_database"
 JSON_FILE_PATH="C:\\Users\\sachi\\OneDrive\\Code\\PythonRecipes\\data.json"
-CLIENT_ID = '3828810cf780430682f13d536a05bc74'
-CLIENT_SECRET = '64892b3cb41e41f1bce64850e1a2f92d'
+CLIENT_ID = 'xxx'
+CLIENT_SECRET = 'xxx'
